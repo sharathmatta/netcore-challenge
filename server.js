a79bebc695dac7da575e62574ef1dfd318f472be
sharath <sharathmatta47@gmail.com>
2022-02-11 12:49:32 +0530

    heroku support

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/cars-form'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+ '/dist/<app-name>/index.html'));});
+res.sendFile(path.join(__dirname+ '/dist/cars-form/index.html'));});
 app.listen(process.env.PORT || 8080);
